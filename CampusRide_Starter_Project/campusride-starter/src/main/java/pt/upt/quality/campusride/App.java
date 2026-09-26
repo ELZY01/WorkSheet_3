914bdacbe146cfa0d4c9bb4a81b5b76f025ae67f
Bruninha9 <brunajasmin15@gmail.com>
2026-09-26 15:03:13 +0100

    feat: update scooter pricing logic and app demo (closes #2)

--- CampusRide_Starter_Project/campusride-starter/src/main/java/pt/upt/quality/campusride/App.java
+++ CampusRide_Starter_Project/campusride-starter/src/main/java/pt/upt/quality/campusride/App.java
@@ -17,6 +17,6 @@ public class App {
     private static void runTeamFeatures(Fleet fleet,
                                         RentalService rentalService,
                                         FleetReport report) {
-        System.out.println("TEAM FEATURES NOT YET INTEGRATED");
+        System.out.println("S10 / 40 min = " + rentalService.calculateRentalPrice("S10", 40) + " EUR");
     }
 }
